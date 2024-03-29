88a701239b025b4edfbbc1f27bf76ac507fe699b
Mikael Rönnberg <mikael.ronnberg@medieinstitutet.se>
2024-03-29 13:52:52 +0100

    Updated start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Center, Flex, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Flex>
+    <Flex w="100vw" h="100vh" justify="center" align="center">
       <Center>
         <Text>The Menu</Text>
       </Center>
